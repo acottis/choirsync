f4ee1e23ce2577ba4c6e270b656a7cc59ad76dd6
Adam Cottis <you@example.com>
2021-03-27 15:14:09 +0000

    Add appengine yaml

---
+++ Client/index.js
@@ -0,0 +1,100 @@
+const button_play = document.getElementById("click_me_play")
+const button_rec = document.getElementById("click_me_record")
+const button_both = document.getElementById("click_me_both")
+const audio_file = 'Music/InDulci.mpeg'
+
+const play_audio = () => {
+    const audio = new Audio(audio_file);
+    audio.play();
+}
+
+
+function download(recordedChunks) {
+    var blob = new Blob(recordedChunks, {
+      type: 'video/webm'
+    });
+    var url = URL.createObjectURL(blob);
+    var a = document.createElement('a');
+    document.body.appendChild(a);
+    a.style = 'display: none';
+    a.href = url;
+    a.download = 'test.webm';
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
+
+
+const rec_audio = () => {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            const mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start();
+
+            const audioChunks = [];
+            mediaRecorder.addEventListener("dataavailable", event => {
+                audioChunks.push(event.data);
+            });
+
+            mediaRecorder.addEventListener("stop", () => {
+                const audioBlob = new Blob(audioChunks);
+                const audioUrl = URL.createObjectURL(audioBlob);
+                const audio = new Audio(audioUrl);
+                audio.play();
+            });
+
+            setTimeout(() => {
+                mediaRecorder.stop();
+            }, 3000);
+        });
+};
+
+const both_audio = () => {
+
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            const audio_song = new Audio(audio_file);
+            audio_song.play();
+
+            const mediaRecorder = new MediaRecorder(stream, {
+                mimeType : 'audio/webm'
+            });
+            mediaRecorder.start();
+
+            const audioChunks = [];
+            mediaRecorder.addEventListener("dataavailable", event => {
+                audioChunks.push(event.data);
+            });
+
+            mediaRecorder.addEventListener("stop", () => {
+                const audioBlob = new Blob(audioChunks);
+                const audioUrl = URL.createObjectURL(audioBlob);
+                const audio_recording = new Audio(audioUrl);
+                console.log(audioChunks)
+                audio_recording.play();
+                download(audioChunks);
+                // fetch(`http://localhost:3000/recording`, {
+                //     method: "POST",
+                //     body: audioChunks
+                // });
+            });
+
+
+            setTimeout(() => {
+                console.log()
+                mediaRecorder.stop();
+                audio_song.pause();
+                audio_song.currentTime = 0;
+            }, 30000);
+        });
+};
+
+
+
+button_play.onclick = play_audio
+button_rec.onclick = rec_audio
+button_both.onclick = both_audio
+
+
+
+
--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,27 @@
-console.log("Hello World")
-
-
 const express = require('express')
+const fs = require('fs')
+
 const app = express()
 const port = 3000
 
+app.use('/static', express.static('Client'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.send("Hello World")
+})
+
+app.post('/recording', (req, res) => {
+   // res.send("Hello World")
+  
+   fs.appendFile('test.mp3', req.body, function(err){
+       console.log(err)
+   })
+//    fs.writeFile('Test.mp3', req.body, function(err) {
+//        console.log(err)
+//    })
+   //res.send(req.body)
 })
 
 
