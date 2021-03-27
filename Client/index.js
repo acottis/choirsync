0e99cf8be775a4ddffa30c98946b20b42b6faa4b
Adam Cottis <you@example.com>
2021-03-27 20:16:42 +0000

    modified:   Client/index.js

--- Client/index.js
+++ Client/index.js
@@ -51,28 +51,56 @@ const rec_audio = () => {
         });
 };
 
+const times = []
+
 const both_audio = () => {
     navigator.mediaDevices.getUserMedia({ audio: true })
         .then(stream => {
 
             audio.play();
+            times[0]=new Date();
+            const start = new Date();
 
             const audioChunks = [];
             const mediaRecorder = new MediaRecorder(stream, {
                 mimeType : 'audio/webm'
             });
 
-            audio.addEventListener("playing", event => {
+            audio.addEventListener("pause", event => {
+                times[1]=new Date();
+                const ended = new Date();
+                const delta = ended - start
+                console.log(delta)
+                var a = document.createElement('a');
+                document.body.appendChild(a);
+                a.innerText = delta;
 
-                
+                const text = `\n ${times[0]*1} audio.play\n
+                ${times[1]*1} pause listener\n
+                ${times[2]*1} playing listener\n
+                ${times[3]*1} mediaRecorder.start\n
+                ${times[4]*1} stop listener\n
+                ${times[5]*1} mediaRecorder.stop\n
+                ${times[6]*1} audio.pause\n`
+                var b = document.createElement('b');
+                document.body.appendChild(b);
+                b.innerText = text;
+
+            })
+
+            audio.addEventListener("playing", event => {
+                times[2]=new Date();
+        
                 console.log(event)
                 mediaRecorder.start();
+                times[3]=new Date();
                 
                 mediaRecorder.addEventListener("dataavailable", event => {
                     audioChunks.push(event.data);
                 });
     
                 mediaRecorder.addEventListener("stop", () => {
+                    times[4]=new Date();
                     const audioBlob = new Blob(audioChunks);
                     const audioUrl = URL.createObjectURL(audioBlob);
                     const audio_recording = new Audio(audioUrl);
@@ -89,7 +117,9 @@ const both_audio = () => {
                 setTimeout(() => {
                     console.log()
                     mediaRecorder.stop();
+                    times[5]=new Date();
                     audio.pause();
+                    times[6]=new Date();
                     audio.currentTime = 0;
                 }, 15000);
             })
