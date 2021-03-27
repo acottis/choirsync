26e4ee453d94ca2464cd9d322b2d9a4582c1de4f
Adam Cottis <you@example.com>
2021-03-27 15:29:28 +0000

    trying to fix appengine issues

--- Server/index.js
+++ Server/index.js
@@ -2,29 +2,24 @@ const express = require('express')
 const fs = require('fs')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8080
 
 app.use('/static', express.static('Client'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.listen(port, () => {
+	console.log(`Listening on: http://localhost:${port}/static`);
+});
+
 
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
 app.post('/recording', (req, res) => {
-   // res.send("Hello World")
+   //res.send("Hello World")
   
-   fs.appendFile('test.mp3', req.body, function(err){
-       console.log(err)
-   })
-//    fs.writeFile('Test.mp3', req.body, function(err) {
-//        console.log(err)
-//    })
-   //res.send(req.body)
 })
 
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
