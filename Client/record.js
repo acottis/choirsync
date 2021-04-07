a27c136d6358477b7bd4973dc56c2d2f564f4a03
Naomi Russell <naomirussell@hotmail.co.uk>
2021-04-07 21:52:55 +0100

    timers to test cross-browser recording

--- Client/record.js
+++ Client/record.js
@@ -79,7 +79,7 @@ const start_recording = () => {
                     }
                     recordings.push(new_recording)
                     add_recording_to_page(recordings.length-1)
-                    //log_times(timers);
+                    log_times(timers);
                     button_rec.className="big_button"
                     button_stop_rec.onclick = null
                     record_mode = false
